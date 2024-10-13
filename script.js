ff1de4db5ec8d1ea3a8ad3701932728b6f569e16
Jumael Ahmed Khan <jumael99@gmail.com>
2024-10-13 20:03:40 +0600

    changed dev2.1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("hello world!"); // naimur
 console.log("from dev2"); // naimur
-console.log("naimur changed from dev2"); // jumael -> naimur
+console.log("jumael changed from dev2"); // jumael -> naimur
 
+console.log("dev2.1");
